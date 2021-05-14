e0c6efbb92ac382c6009078dfa9bbb935a1b4be1
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-05-13 19:35:06 -0500

    betty

--- head.c
+++ head.c
@@ -8,41 +8,41 @@
  */
 int main(int argc, char *argv[])
 {
-  FILE *op = NULL;
-  stack_t *stack = NULL;
-  char *command = NULL, *space = NULL;
-  size_t lineWeight = 0;
-  unsigned int number_line = 0;
+FILE *op = NULL;
+stack_t *stack = NULL;
+char *command = NULL, *space = NULL;
+size_t lineWeight = 0;
+unsigned int number_line = 0;
 
-  if (argc != 2)
-    {
-      fprintf(stderr, "USAGE: monty file\n");
-      return (EXIT_FAILURE);
-    }
-  op = fopen(argv[1], "r");
-  if (op == NULL)
-    {
-      fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
-  while (getline(&space, &lineWeight, op) != EOF)
-    {
-      number_line++;
-      command = strtok(space, DELIMITERS);
-      if (command == NULL)
-	{
-	  free(command);
-	  continue;
-	}
-      else if (*command == '#')
-	{
-	  continue;
-	}
-      opcode_stack(command, &stack, number_line);
-    }
-  free(space);
-  fclose(op);
-  free_stack(stack);
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+return (EXIT_FAILURE);
+}
+op = fopen(argv[1], "r");
+if (op == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+while (getline(&space, &lineWeight, op) != EOF)
+{
+number_line++;
+command = strtok(space, DELIMITERS);
+if (command == NULL)
+{
+free(command);
+continue;
+}
+else if (*command == '#')
+{
+continue;
+}
+opcode_stack(command, &stack, number_line);
+}
+free(space);
+fclose(op);
+free_stack(stack);
 
-  return (0);
+return (0);
 }
